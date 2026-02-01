6930ffa78a6df9542da51c0bebca2a25857b119f
SR-Extreme <saurav0808roy@gmail.com>
2026-02-02 03:20:16 +0530

    Signup/login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,13 +6,19 @@ import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import Footer from './components/Footer/Footer'
 import AppDownload from './components/AppDownload/AppDownload'
+import LoginPopup from './components/LoginPopup/LoginPopup'
+import { useState } from 'react'
 
 
 const App = () => {
+
+  const[showLogin,setShowLogin] = useState(false);
+
   return (
     <>
+    {showLogin ? <LoginPopup setShowLogin={setShowLogin}/> : <></>}
     <div className='app'>
-      <Navbar/>
+      <Navbar setShowLogin={setShowLogin}/>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/cart' element={<Cart/>}></Route>
