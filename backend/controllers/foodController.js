308c6e727406fa796a57318b4683041245b757bb
SR-Extreme <saurav0808roy@gmail.com>
2026-02-13 16:23:39 +0530

    DashBoard Functionalities

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -16,7 +16,7 @@ const addFood = async (req,res) => {
     })
     try {
         await food.save();
-        res.json({success:true,message:"Food Added"})
+        res.json({success:true,message:"Food Added Successfully !!"})
     } catch(error) {
         console.log(error)
         res.json({success:false,message:error});
